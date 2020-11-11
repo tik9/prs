99d8686db6ede26fdd2131b8dd9b8677b6cf2741
tik <lenovo>
2020-11-11 11:25:16 +0100

    Newest commit

--- gitmanager.ps1
+++ gitmanager.ps1
@@ -5,7 +5,7 @@ function gi_do ($verz) {
     Write-Output "\n[**** update Git: " $verz.fullname
     # git status
     # git pull
-    git add .
+    # git add .
     git commit -am $commit
     git push
 }
@@ -19,13 +19,14 @@ foreach ($verz in $gd_find) {
     # Write-Output dire $(Get-ChildItem $verz)
     if ('.git' -in (Get-ChildItem -Path $verz).name) {
         # Write-Output verz $verz
-        # gi_do $verz
+        gi_do $verz
     }
 }
 
 $ar = 
 # $cs, 
-$prof_home, $zsh_cu
+$prof_home
+,$zsh_cu
 ,$doks
 
 foreach ($elem in $ar) {
